fc6e88145fd7adb5fc11094ed596e23bdc7fdab2
chadhas282-afk <chadhas282@gmail.com>
2026-05-04 12:01:31 +0530

    feat: initialize socket.io server setup in server.js

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo').default;
 const mongoose = require('mongoose');
 const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
